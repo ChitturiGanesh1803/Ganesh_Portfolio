4fee0e35582ae7ba69fe6675501bab4d6d840472
ChitturiGanesh1803 <117992994+ChitturiGanesh1803@users.noreply.github.com>
2025-02-24 14:08:51 +0530

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -8,7 +8,7 @@ function Hero() {
             <div class="row justify-content-center text-center">
                 <div class="col-md-8">
                     <h1 class="display-4 mb-4"><i class="fas fa-user-circle"></i> Welcome to my portfolio!</h1>
-                    <p class="lead mb-4">I'm Sai Ganesh Kumar Chitturi, a Full Stack Python Developer passionate about cybersecurity and AI.</p>
+                    <p class="lead mb-4">I'm Sai Ganesh Kumar Chitturi, a Full Stack Python Developer. I have developed an **Ethical Hacking project** using **HTML, CSS, JavaScript, Django, and MySQL**, focusing on building secure, dynamic, and responsive web applications.</p>
                     <div class="social-links">
                         <a href="mailto:chitturiganesh1803@gmail.com"><i class="fas fa-envelope"></i></a>
                         <a href="https://www.linkedin.com/in/ganesh-chitturi-9909ab287" target="_blank"><i class="fab fa-linkedin"></i></a>
